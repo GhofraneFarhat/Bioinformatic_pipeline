b9fe776bdf01c42db72d05ccfa68952f08a85b1a
GhofraneFARHAT <ghofrane.farhat@ensi-uma.tn>
2024-05-29 11:25:23 +0200

    treat the src folder as containing packages

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# This is in __init__.py
+
+# This code will be run when the package is imported.
+print("Package is being initialized.")
+
+# __all__ will define what is imported with "from package import *"
+__all__ = ['module1', 'module2']
