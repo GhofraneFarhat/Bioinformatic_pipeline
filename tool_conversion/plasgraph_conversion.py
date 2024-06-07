f211d395e4083df5a89be0aaf15e3aecfa829e82
GhofraneFARHAT <ghofrane.farhat@ensi-uma.tn>
2024-06-07 05:03:45 +0200

    some changements

--- tool_conversion/plasgraph_conversion.py
+++ tool_conversion/plasgraph_conversion.py
@@ -10,16 +10,7 @@ class CsvToCsv:
 
     def convert(self):
         
-        shutil.copy(self.input_file, self.output_file)
-        return self.output_file
+        
+        return self.input_file
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='CSV to CSV')
-    parser.add_argument('csv_file_input', type=str, help='Path to the input csv file')
-    parser.add_argument('csv_file_output', type=str, help='Path to the output csv file')
-
-    args = parser.parse_args()
-
-    conversion = CsvToCsv(args.csv_file_input, args.csv_file_output)
-    res = conversion.convert()
