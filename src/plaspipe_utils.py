a0cbf29e69a9e2768e60a0e59254d2a3c694a93a
GhofraneFARHAT <ghofrane.farhat@ensi-uma.tn>
2024-06-18 05:17:44 +0200

    add the function for formatting the input to the plasbin-flow tool

---
+++ src/plaspipe_utils.py
@@ -0,0 +1,29 @@
+import os
+
+def csv_to_tsv(csv_file_path):
+    """
+    Convert a CSV file to a TSV file with the same name and path, only changing the file extension.
+
+    Args:
+        csv_file_path (str): Path to the input CSV file.
+
+    Returns:
+        str: Path to the output TSV file.
+    """
+    # Get the directory and filename from the input CSV file path
+    directory, filename = os.path.split(csv_file_path)
+
+    # Create the output TSV filename by replacing the extension
+    tsv_filename = os.path.splitext(filename)[0] + '.tsv'
+
+    # Join the directory and TSV filename to get the output TSV file path
+    tsv_file_path = os.path.join(directory, tsv_filename)
+
+    # Convert the CSV file to TSV
+    with open(csv_file_path, 'r') as csv_file, open(tsv_file_path, 'w', newline='') as tsv_file:
+        next(csv_file)
+        for line in csv_file:
+            tsv_file.write(line.replace(',', '\t'))
+
+    return tsv_file_path
+
