dc82e839928c0f4655b565ff819507e760375b70
GhofraneFARHAT <ghofrane.farhat@ensi-uma.tn>
2024-06-20 11:00:43 +0200

    add exception for creating directory, input files and args

--- src/plaspipe.py
+++ src/plaspipe.py
@@ -1,8 +1,5 @@
 import yaml
-from .classification_wrapper import ClassificationWrapper
 import os
-from .plaspipe_data import PipelineData
-from .binning_wrapper import BinningWrapper
 import json
 import time
 import argparse
@@ -10,43 +7,66 @@ import gzip
 import sys
 import shutil
 import logging
+from Bio import SeqIO
+
+from .plaspipe_utils import process_exception
+from .plaspipe_utils import check_file
+from .plaspipe_utils import gunzip_FASTA
+from .plaspipe_utils import gunzip_GFA
+from .plaspipe_utils import create_directory
+
+
+from .plaspipe_data import PipelineData
 
 
 
 def parse_arguments():
-    """ 
-    reading the argument from the command line
-    return: args: list of arguments
+
+    """
+    Read arguments from the command line.
+    Returns: args (list): List of arguments.
     """
 
     # Create the argument parser
     parser = argparse.ArgumentParser(description='Pipeline for bioinformatic tools (classification and binning tools for plasmid analysis')
-
-    # Add arguments
-    parser.add_argument('user_file', type=str, help='path to the yaml file that the user with provide it to the pipeline')
-    #parser.add_argument('pipeline_input_file', type=str, help='path to pipeline input file, this input file can be fasta or GFA or both ')
-    parser.add_argument('pipeline_output', type=str, help='the file that the contigs dict and bins dict will be saved ther, the output of the pipeline')
-
-    # Parse the command-line arguments
+    parser.add_argument('user_file', type=str, help='Path to the YAML file provided by the user')
+    parser.add_argument('pipeline_output', type=str, help='File to save contigs dict and bins dict (pipeline output)')
     args = parser.parse_args()
+
+    # Verify the number of arguments
+    if len(sys.argv) != 3:
+        process_exception("Invalid number of arguments. Usage: python script.py user_file.yaml output_file.txt")
+
+    # Verify the user_file is a YAML file
+    if not args.user_file.endswith('.yaml'):
+        process_exception(f"Invalid user file '{args.user_file}'. Expected a YAML file.")
+
+
 
     return args
 
 
 # the yaml file, from the user need some changes
 def load_yaml(file):
-    """ load the yaml file """
-    """ 
-    Args: file: the yaml file
-    Returns: contigs
+
     """
-    with open(file, 'r') as yaml_file:
-        configs = yaml.safe_load(yaml_file)
-    return configs
+    Load the YAML file.
+    Args:
+        file (str): Path to the YAML file.
+    Returns:
+        configs (dict): Configurations from the YAML file.
+    """
+
+    try:
+        with open(file, 'r') as yaml_file:
+            configs = yaml.safe_load(yaml_file)
+        if not configs:
+            process_exception(f'YAML file {file} is empty.')
+        return configs
+    except Exception as e:
+        process_exception(f'Error loading YAML file {file}: {e}')
 
 
-#the input file of the pipeline can be fasta or gfa or both
-#need to change, the inpute of the pipeline will be provided from the yaml file
 def get_input_file(method_config):
 
     outdir_pipeline = method_config['outdir_pipeline']
@@ -59,118 +79,101 @@ def get_input_file(method_config):
     fasta_path = method_config['input']['path_to_input_fasta']
     print(f"this is my gfa file {gfa_path}")
 
-    if gfa_path and gfa_path.endswith('.gz'):
-        gfa_path = gunzip_GFA(gfa_path, gun_gfa_path)
-        
-            
-    if fasta_path and fasta_path.endswith('.gz'):
-        fasta_path = gunzip_FASTA(fasta_path, gun_fasta_path)
+
+    #unzip the gfa file
+    try:
+        if gfa_path and gfa_path.endswith('.gz'):
+            gfa_path = gunzip_GFA(gfa_path, gun_gfa_path)
+    except Exception as e:
+        logging.error(f"Error unzipping GFA file {gfa_path}: {e}")
+
+
+    #unzip the fasta file
+    try:
+        if fasta_path and fasta_path.endswith('.gz'):
+            fasta_path = gunzip_FASTA(fasta_path, gun_fasta_path)
+    except Exception as e:
+        logging.error(f"Error unzipping FASTA file {fasta_path}: {e}")
+
 
     return gfa_path, fasta_path
 
-#gunzzipping a gfa file
-def gunzip_GFA(in_file_path, out_file_path):
+
+def run_plaspipe_data(method_configs, prefix, class_tool_name, class_tool_version, bin_tool_name, bin_tool_version, input_gfa, input_fasta, classification_dir, binning_dir):
+            
+    #cretae the plaspipe_data instance
+    plaspipe_data = PipelineData(method_configs, prefix, class_tool_name, class_tool_version, bin_tool_name, bin_tool_version, input_gfa, input_fasta, classification_dir, binning_dir)
+
+    #load contigs name
+    plaspipe_data.load_contigs()
+
+    #update the plaspipe_data
+    plaspipe_data.update_plaspipe_data_from_classwrapper()
+
+    #update the plaspipe_data
+    plaspipe_data.update_plaspipe_data_from_binwrapper()
+
+    return plaspipe_data
+
+
+#pipeline configs 
+def get_pipeline_configs(method_configs):
     """
-    Gunzip a GFA file
+    Extract pipeline configurations from the method_configs dictionary.
 
     Args:
-       in_file_path (str): path to input gzipped GFA file
-       out_file_path (str): path to output GFA file
+        method_configs (dict): Configuration dictionary for the pipeline.
 
     Returns:
-       Creates GFA file out_file_path
+        list: A list containing the extracted configurations.
     """
-    try:
-        with gzip.open(in_file_path) as in_file, open(out_file_path, 'wb') as out_file:
-            shutil.copyfileobj(in_file, out_file)
-            print(f'this my gfa file gunzipped {out_file_path}')
-            return out_file_path
-    except Exception as e:
-        process_exception(f'FASTA\tGunzipping {in_file_path} to {out_file_path}: {e}')
+    pipeline_configs = []
 
-#gunzip a fasta file
-def gunzip_FASTA(in_file_path, out_file_path):
-    """
-    Gunzip a FASTA file
+    # Extract output directories
+    classification_dir = method_configs['classification']['output']['outdir_classification']
+    binning_dir = method_configs['binning']['output']['outdir_binning']
+    prefix = method_configs['prefix']
+    pipeline_configs.extend([classification_dir, binning_dir, prefix])
 
-    Args:
-       in_file_path (str): path to input gzipped FASTA file
-       out_file_path (str): path to output FASTA file
+    # Extract tool information
+    class_tool_name = method_configs['classification']['name']
+    class_tool_version = method_configs['classification']['version']
+    class_input_format = method_configs['classification']['input_format']
+    bin_tool_name = method_configs['binning']['name']
+    bin_tool_version = method_configs['binning']['version']
+    bin_input_format = method_configs['binning']['input_format']
+    pipeline_configs.extend([class_tool_name, class_tool_version, class_input_format,
+                             bin_tool_name, bin_tool_version, bin_input_format])
 
-    Returns:
-       Creates FASTA file out_file_path
-    """
-    records = []
-    with gzip.open(in_file_path, 'rt') as handle:
-        for record in SeqIO.parse(handle, 'fasta'):
-            records.append(record)
-        with open(out_file_path, 'w') as out_file:
-            SeqIO.write(records, out_file, 'fasta')
+    # Extract output directory for the pipeline
+    out_dir = method_configs['outdir_pipeline']
+    pipeline_configs.append(out_dir)
 
-def process_exception(msg):
-    logging.exception(msg)
-    print(f'EXCEPTION\t{msg}', file=sys.stderr)
-    sys.exit(1)
-
-
-
-
-#the function to run the classification wrapper
-def run_classification(classification_folder, prefix, config, input_fasta, input_gfa, class_name, class_version):
-
-    """ run the classifcation wrapper"""
-    """ 
-    Args: 
-    config: the configuration of the classification tool
-    repo_path : the path of the classification tool folder
-    conv_file: the conversion file, the result of the conversion function
-    input_fasta, input_gfa: the input of the pipeline
-    Returns:
-    plaspipe_data: the data of the pipeline updated after passing by the classification tool
-    """
-
-
-    #create the classwrapper instance
-    classification_wrapper = ClassificationWrapper(classification_folder, prefix, config, input_fasta, input_gfa)
-
-    #run the classification wrapper 
-    classification_wrapper.run()
-
-
-    #return the plaspipe_data after the changement of the classification wrapper, add contigs class
-    return classification_wrapper
-
-#the function to run the binning wrapper
-def run_binning(binning_folder, prefix, config, input_fasta, input_gfa, classification_path, class_tool_name, class_tool_version):
-    
-    print(f'classification folder {classification_path}')
-    binning_wrapper = BinningWrapper(binning_folder, prefix, config, input_fasta, input_gfa, classification_path, class_tool_name, class_tool_version)
-
-    binning_wrapper.run()
-
-    return binning_wrapper
-
-
-
-
+    return pipeline_configs
 
 
 #the function to generate the pipeline output file of the pipeline
-def generate_output_file(pipeline_data, pipeline_output_file, out_dir, contigs, bins):
-    """ 
-    this function is to generate the pipeline result saved in a file
-    Args:
-    pipeline_data: the flow of data that is updated all over the pipeline
-    pipeline_output_file: the output file for the command line"bin_tool
-    temp_dir: the path where the output file will be saved
+def generate_output_file(pipeline_data, pipeline_output_file, out_dir):
+
     """
+    Generate the pipeline output file.
+
+    Args:
+        pipeline_data (PipelineData): Instance of the PipelineData class.
+        pipeline_output_file (str): Name of the output file of the pipeline
+        out_dir (str): Directory to save the output file.
+    """
+
     if out_dir is None:
         out_dir = os.getcwd()
         print(out_dir)
 
     # Create the directory if it doesn't exist
-    os.makedirs(out_dir, exist_ok=True)
+    create_directory(out_dir) 
 
+    # Get the contigs and bins from the PipelineData instance
+    contigs = pipeline_data.get_contigs()
+    bins = pipeline_data.get_bins()
 
     #the path to the file result of the pipeline
     output_file = os.path.join(out_dir, pipeline_output_file)
@@ -189,76 +192,35 @@ def generate_output_file(pipeline_data, pipeline_output_file, out_dir, contigs, 
     print(f"Results saved to {output_file}")
 
 def main():
-    args = parse_arguments()
-
-    method_configs = load_yaml(args.user_file)
-
-    #the pipeline input file
-    #need to figure out if the user provide two inputs (fa and gfa)
-    input_gfa, input_fasta = get_input_file(method_configs)
-
-    #extract the temp directory of the output different format of the classification tool
-    classification_dir = method_configs['classification']['output']['outdir_classification']
-    binning_dir = method_configs['binning']['output']['outdir_binning']
-    prefix = method_configs['prefix']
-    
-    print(f'the classifcation_dir {classification_dir}')
-    
-    
-    
-    class_tool_name = method_configs['classification']['name']
-    class_tool_version = method_configs['classification']['version']
-
-    bin_tool_name = method_configs['binning']['name']
-    bin_tool_version = method_configs['binning']['version']
 
 
+    try:
+        args = parse_arguments()
+        method_configs = load_yaml(args.user_file)
 
+        # Get the pipeline input files
+        input_gfa, input_fasta = get_input_file(method_configs)
 
-    if (method_configs['classification']['input_format'] == 'GFA' or method_configs['binning']['input_format'] == 'GFA') and (input_gfa == None):
-        print("this pipeline can't handle the conversion of fasta to GFA")
-    else:
+        # Get pipeline configurations
+        plaspipe_configs = get_pipeline_configs(method_configs)
 
-        #create the instance of plaspipe_data
-        #plaspipe_data = PipelineData(class_tool_name, class_tool_version, input_gfa, input_fasta)
-        
-        #cretae the plaspipe_data instance
-        plaspipe_data = PipelineData(method_configs, prefix, class_tool_name, class_tool_version, bin_tool_name, bin_tool_version, input_gfa, input_fasta, classification_dir, binning_dir)
+        # Unpack the configurations
+        classification_dir, binning_dir, prefix, class_tool_name, class_tool_version, class_input_format, bin_tool_name, bin_tool_version, bin_input_format, out_dir = plaspipe_configs
 
-        #load contigs name
-        plaspipe_data.load_contigs()
+        print(f'Classification output directory: {classification_dir}')
 
+        # Check if the pipeline can handle the input format
+        if (class_input_format == 'GFA' or bin_input_format == 'GFA') and (input_gfa is None):
+            print("This pipeline cannot handle the conversion of FASTA to GFA")
+        else:
+            # Create the PipelineData instance
+            plaspipe_data = run_plaspipe_data(method_configs, prefix, class_tool_name, class_tool_version, bin_tool_name, bin_tool_version, input_gfa, input_fasta, classification_dir, binning_dir)
 
-        #run the classiifcation wrapper function
-        #classification_wrapper = run_classification(classification_dir, prefix, method_configs['classification'], input_fasta, input_gfa, class_tool_name, class_tool_version )
+            # Generate the pipeline output file
+            generate_output_file(plaspipe_data, args.pipeline_output, out_dir)
 
-        #cretae the plaspipe_data instance
-        #plaspipe_data = PipelineData(classification_wrapper, "", class_tool_name, class_tool_version, input_gfa, input_fasta)
-
-        #update the plaspipe_data
-        plaspipe_data.update_plaspipe_data_from_classwrapper()
-
-        #the result of the classification tool
-        contigs = plaspipe_data.get_contigs()
-
-        #run the binning wrapper function
-        #binning_wrapper = run_binning(binning_dir, prefix, method_configs['binning'], input_fasta, input_gfa, classification_dir, class_tool_name, class_tool_version)
-
-        #cretae the plaspipe_data instance
-        #plaspipe_data = PipelineData(classification_wrapper, binning_wrapper, class_tool_name, class_tool_version, input_gfa, input_fasta)
-
-        #update the plaspipe_data
-        plaspipe_data.update_plaspipe_data_from_binwrapper()
-
-        #the result of the binning tool
-        bins = plaspipe_data.get_bins()
-
-        #run the plaspipe function
-        #plaspipe_data = run_plaspipe_data(classification_wrapper, binning_wrapper, class_tool_name, class_tool_version, input_gfa, input_fasta)
-
-        #for the output of the pipeline
-        out_dir = method_configs['outdir_pipeline']
-        generate_output_file(plaspipe_data, args.pipeline_output, out_dir, contigs, bins)
+    except Exception as e:
+        process_exception(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
--- src/plaspipe_utils.py
+++ src/plaspipe_utils.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import sys
 
 def csv_to_tsv(csv_file_path):
     """
@@ -32,3 +33,81 @@ def process_exception(msg):
     logging.exception(msg)
     print(f'EXCEPTION\t{msg}', file=sys.stderr)
     sys.exit(1)
+
+def process_error(msg):
+    logging.error(msg)
+    print(f'ERROR\t{msg}', file=sys.stderr)
+    sys.exit(1)
+
+def process_warning(msg):
+    logging.warning(msg)
+    print(f'WARNING\t{msg}', file=sys.stderr)
+
+
+def _check_file(in_file, log=False, msg='FILE'):
+    try:
+        if not os.path.isfile(in_file):
+            raise CustomException('File is missing')
+        elif os.path.getsize(in_file) == 0:
+            process_warning(f'{msg}\t{in_file}: is empty')
+    except Exception as e:
+        process_exception(f'{msg}\t{in_file}: {e}')
+    else:
+        if log:
+            logging.info(f'{msg}\t{in_file}')
+            
+def check_file(in_file):
+    _check_file(in_file, log=False)
+
+
+#gunzzipping a gfa file
+def gunzip_GFA(in_file_path, out_file_path):
+    """
+    Gunzip a GFA file
+
+    Args:
+       in_file_path (str): path to input gzipped GFA file
+       out_file_path (str): path to output GFA file
+
+    Returns:
+       Creates GFA file out_file_path
+    """
+    try:
+        with gzip.open(in_file_path) as in_file, open(out_file_path, 'wb') as out_file:
+            shutil.copyfileobj(in_file, out_file)
+            print(f'this my gfa file gunzipped {out_file_path}')
+            return out_file_path
+    except Exception as e:
+        process_exception(f'FASTA\tGunzipping {in_file_path} to {out_file_path}: {e}')
+
+
+#gunzip a fasta file
+def gunzip_FASTA(in_file_path, out_file_path):
+    """
+    Gunzip a FASTA file
+
+    Args:
+       in_file_path (str): path to input gzipped FASTA file
+       out_file_path (str): path to output FASTA file
+
+    Returns:
+       Creates FASTA file out_file_path
+    """
+    records = []
+    with gzip.open(in_file_path, 'rt') as handle:
+        for record in SeqIO.parse(handle, 'fasta'):
+            records.append(record)
+        with open(out_file_path, 'w') as out_file:
+            SeqIO.write(records, out_file, 'fasta')
+
+def create_directory(directory_path):
+    """
+    Create a directory if it doesn't exist.
+
+    Args:
+        directory_path (str): Path to the directory.
+    """
+    try:
+        os.makedirs(directory_path, exist_ok=True)
+    except OSError as e:
+        process_exception(f"Error creating directory '{directory_path}': {e}")
