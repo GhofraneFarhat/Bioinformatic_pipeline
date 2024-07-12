94af1e1635a5c936054b2f6c293e530f4e0c6076
Ghofrane Farhat <ghofranefarhat1234@gmail.com>
2024-07-13 00:54:49 +0200

    command changement

--- pipeline/plaspipe/src/plasbin_utils.py
+++ pipeline/plaspipe/src/plasbin_utils.py
@@ -18,12 +18,17 @@ def generate_seed_contigs(gfa_file, mapping_file, method_configuration):
     binning_outdir = method_configuration['output']['outdir_binning']
     out_dir = method_configuration.get('plasbin_out_dir', binning_outdir)
 
-    
+    python_executable = sys.executable
     seed_contigs_script = os.path.join(absolute_path(), 'submodules/PlasBin/code/generate_seeds.py')
        
 
-    command = ["python", seed_contigs_script, "--ag", gfa_file, "--map", mapping_file, "--out", out_dir]
-
+    command = [
+        python_executable,
+        seed_contigs_script,
+        '--ag', gfa_file,
+        '--map', mapping_file,
+        '--out', out_dir,
+    ]
 
     print(f'command to run seed_contigs_file {command}')
     subprocess.run(command, check=True)
