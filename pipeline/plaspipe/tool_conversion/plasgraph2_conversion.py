1039bf50d373fbd4be86a5ebb7d9e7b7a1bec4b3
Ghofrane Farhat <ghofranefarhat1234@gmail.com>
2024-07-20 07:22:55 +0200

    eliminate the sample field

--- pipeline/plaspipe/tool_conversion/plasgraph2_conversion.py
+++ pipeline/plaspipe/tool_conversion/plasgraph2_conversion.py
@@ -18,13 +18,13 @@ class Plasgraph2ToCsv:
         # Swap the columns
         new_data = []
         for row in data[1:]:  # Skip the header row
-            new_row = [row[1], row[3],row[2], row[4], row[5], row[0] ]
+            new_row = [row[1], row[3],row[2], row[4], row[5]]
             new_data.append(new_row)
 
         # Write the new CSV file
         with open(self.output_file, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['contig_name','plasmid_score','length','chromosome_score','label','sample'])  # Write the new header
+            writer.writerow(['contig_name','plasmid_score','length','chromosome_score','label'])  # Write the new header
             writer.writerows(new_data)
 
 
